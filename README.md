853c0d28a400fa97bb3819758d0075a4184a0e5b
bob chase <bob.chase@gmail.com>
2017-12-17 09:48:22 -0500

    initial release

---
+++ README.md
@@ -0,0 +1,168 @@
+# FSM
+
+### Finite State Machine for Python
+
+The `fsm` project allows a state-event machine to be defined and executed
+in python code.
+
+The `fsm` is defined in a mini-language which is compiled to python code,
+allowing it to respond to `events` and call `action` routines in accordance with
+the state machine definition.
+
+### An example
+
+Here is a trivial state machine for a push-button light switch.
+
+```
+STATE off
+  EVENT press on
+    ACTION turn_on
+STATE on
+  EVENT press off
+    ACTION turn_off
+```
+
+The machine starts in the state `off`. When the `press` event arrives, the action
+`turn_on` is executed, and the machine transisions to the state `on`.
+When the `press` event arrives in the state `on`, the action `turn_off` is
+executed, and the machine transistions back to the state `off`.
+
+This example demonstrates the key ideas of a finite state machine:
+
+* `events` arrive while the machine is in a `state`
+* `events` can trigger an `action` or series of `actions`
+* `events` can trigger a transition to a different `state`
+
+Let's add some more logic to this machine:
+
+```
+STATE off
+  EVENT press on
+    ACTION turn_on
+STATE on
+  ENTER test
+  EVENT on
+  EVENT off off
+  EVENT press off
+    ACTION turn_off
+```
+
+A special `enter` `action` has been added to the state `on`.
+The `enter` action is triggered when a state transition causes a `state` to be entered.
+(An `exit` action can also be specified, to be triggered when a state transition causes a `state` to be exited).
+This action tests the light and returns an event `on` or `off` depending
+upon the state of the light. Nothing happens if the event `on` arrives; if the
+event `off` arrives, the machine transitions of state `off`.
+It is assumed that the `test` `action` emits an `event` of either `on` or `off`.
+
+The same logic can be defined in this way:
+
+```
+STATE off
+  EVENT press
+    ACTION turn_on
+    ACTION test
+  EVENT on on
+  EVENT off
+STATE on
+  EVENT press off
+    ACTION turn_off
+```
+
+The `test` action is executed as part of the `press` event in state `off`, and
+a transition to state `on` only happens if the `test` action returns an `on` event.
+
+A key takeaway from comparing these two state machines is that the `test` action
+works in either state. Action routines are meant to be very small pieces of
+code which operate without regard to state.
+
+The if/elif/else logic which would normally accompany these state management
+problems is abstracted into the `fsm` definition, and the coding is simplified
+into a collection of focused action routines.
+
+### So, how does this work with python?
+
+##### Parsing
+
+If we have an fsm description file named `switch.fsm` in a module
+named `my_switch`. The unix path to this file is, starting with the top level
+directory of the module, `my_switch/switch.fsm`. The following code will parse this file:
+```
+from fsm.parser import Parser as parser
+description = parser.parse('my_switch.switch.fsm')
+```
+
+Using dot-notation to specify the location within the module allows the fsm description
+file to be located anywhere in `PYTHONPATH`.
+
+The `parse` function will also accept an os-specific file path, or a list of strings,
+where each string acts as a line from a file.
+```
+description = parser.parse([
+    'STATE off',
+    '  EVENT press on',
+    '    ACTION turn_on',
+    'STATE on',
+    '  EVENT press off',
+    '    ACTION turn_off',
+])
+```
+It is best practice to create keep the `fsm` description in a separate file within
+the python directory structure, accessed with dot-notation.
+Tests, or other special cases, may find the
+alternative methods helpful.
+
+##### Actions
+
+Once an `fsm` file has been parsed, `action` routines must be written for each
+`action`. The actions are obvious by inpsection, but can be easily gotten
+from the parsed description:
+```
+>>> description.actions
+['turn_off', 'turn_on']
+```
+
+Here are two trivial implementations:
+```
+def turn_off():
+    print('the light is off')
+
+def turn_on():
+    print('the light is on')
+```
+
+##### Initalize the machine
+
+Combine the description and the actions to create a new machine:
+
+```
+fsm = description.build(
+    turn_off=turn_off,
+    turn_on=turn_on,
+)
+```
+
+##### Run the machine
+
+The machine accepts events with the `handle` method. If `handle` is able to
+process the `event` in the current state, it returns `True`.
+
+Let's try it out:
+```
+>>> fsm.handle('press')
+the light is on
+True
+>>> fsm.handle('press')
+the light is off
+True
+>>> fsm.handle('press')
+the light is on
+True
+```
+Notice that the `event` is simply a string.
+
+If we pass an unrecognized event:
+```
+>>> fsm.handle('huh')
+False
+```
---
+++ fsm/parser.py
@@ -0,0 +1,197 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2013-2017 Robert H Chase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+from file_util.load_from_path import load_from_path
+from fsm.fsm_machine import create as create_machine
+import fsm.FSM as FSM
+
+
+class Parser(object):
+
+    def __init__(self):
+        self.error = None
+        self.states = {}
+        self.first_state = None
+        self.state = None
+        self.event = None
+        self._actions = {}
+        self.fsm = create_machine(
+            action=self.act_action,
+            enter=self.act_enter,
+            event=self.act_event,
+            exit=self.act_exit,
+            state=self.act_state,
+        )
+        self.fsm.state = 'init'
+
+    def __str__(self):
+        states = self.states
+        d = 'from fsm.FSM import STATE, EVENT, FSM\n'
+        d += '\n'.join('# ' + a for a in sorted(self._actions))
+        d += '\ndef create(**actions):\n'
+        d += '\n'.join(self.define(s) for s in states.values())
+        d += '\n' + '\n'.join(self.set_events(s) for s in states.values())
+        d += '\n  return FSM([' + ','.join('S_' + s for s in states) + '])'
+        return d
+
+    @property
+    def actions(self):
+        return sorted(self._actions.keys())
+
+    @property
+    def events(self):
+        events = []
+        for state in self.states.values():
+            for event in state['events'].values():
+                events.append(event['name'])
+        return list(set(events))
+
+    @classmethod
+    def parse(cls, data):
+        data = load_from_path(data, 'fsm')
+        parser = cls()
+        for num, line in enumerate(data, start=1):
+            line = line.split('#', 1)[0].strip()
+            if len(line):
+                line = line.split(' ', 1)
+                if len(line) == 1:
+                    raise Exception('too few tokens, line=%d' % num)
+
+                event, parser.line = line
+                if not parser.fsm.handle(event.lower()):
+                    raise Exception("Unexpected directive '%s', line=%d" % (event, num))
+                if parser.error:
+                    raise Exception('%s, line=%d' % (parser.error, num))
+        return parser
+
+    def build(self, **actions):
+        states = {}
+        for state in self.states.values():
+            s = FSM.STATE(
+                name=state['name'],
+                enter=actions[state['enter']] if state['enter'] else None,
+                exit=actions[state['exit']] if state['exit'] else None,
+            )
+            states[s.name] = s
+            for event in state['events'].values():
+                e = FSM.EVENT(
+                    name=event['name'],
+                    actions=[actions[n] for n in event['actions']],
+                    next_state=event['next'],
+                )
+                s.events[e.name] = e
+        for state in states.values():
+            for event in state.events.values():
+                if event.next_state:
+                    event.next_state = states[event.next_state]
+        fsm = FSM.FSM(states.values())
+        fsm.state = self.first_state
+        return fsm
+
+    @staticmethod
+    def define(state):
+        s = "  S_%s=STATE('%s'" % (state['name'], state['name'])
+        if state['enter']:
+            s += ",enter=actions['%s']" % state['enter']
+        if state['exit']:
+            s += ",exit=actions['%s']" % state['exit']
+        return s + ')'
+
+    @staticmethod
+    def set_events(state):
+        s = "  S_%s.set_events([" % state['name']
+        for e in state['events'].values():
+            s += "EVENT('%s',[" % e['name']
+            s += ','.join("actions['%s']" % a for a in e['actions'])
+            s += ']'
+            if e['next']:
+                s += ', S_%s' % e['next']
+            s += "),"
+        s += '])'
+        return s
+
+    def act_state(self):
+        if len(self.line.split()) != 1:
+            self.error = 'STATE name must be a single token'
+            return 'error'
+        name = self.line.strip()
+        if name in self.states.keys():
+            self.error = 'duplicate STATE name'
+            return 'error'
+        if self.first_state is None:
+            self.first_state = name
+        self.state = dict(name=name, enter=None, exit=None, events={})
+        self.states[name] = self.state
+
+    def act_enter(self):
+        if len(self.line.split()) != 1:
+            self.error = 'ENTER action must be a single token'
+            return 'error'
+        name = self.line.strip()
+        if self.state['enter'] is not None:
+            self.error = 'only one ENTER action allowed'
+            return 'error'
+        self.state['enter'] = name
+        self._actions[name] = True
+
+    def act_exit(self):
+        if len(self.line.split()) != 1:
+            self.error = 'EXIT action must be a single token'
+            return 'error'
+        name = self.line.strip()
+        if self.state['exit'] is not None:
+            self.error = 'only one EXIT action allowed'
+            return 'error'
+        self.state['exit'] = name
+        self._actions[name] = True
+
+    def act_event(self):
+        if len(self.line.split()) == 2:
+            name, next_state = self.line.strip().split()
+        elif len(self.line.split()) != 1:
+            self.error = 'EVENT can only have one or two tokens'
+            return 'error'
+        else:
+            name = self.line.strip()
+            next_state = None
+        self.event = dict(name=name, actions=[], next=next_state)
+        self.state['events'][name] = self.event
+
+    def act_action(self):
+        if len(self.line.split()) != 1:
+            self.error = 'ACTION can only have one token'
+            return 'error'
+        name = self.line.strip()
+        if name in self.event['actions']:
+            self.error = 'duplicate ACTION name'
+            return 'error'
+        self.event['actions'].append(name)
+        self._actions[name] = True
+
+
+if __name__ == '__main__':
+    import sys
+
+    f = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
+    fsm = Parser.parse(f.readlines())
+    print fsm
