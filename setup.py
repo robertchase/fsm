a75e28de25cd2b6d0a0f7c0f0bf3a8d35d3f5e5d
bob chase <${SETUP_GIT_EMAIL:-bob.chase@gmail.com}>
2018-07-29 08:48:22 -0700

    1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='fsm',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(exclude=['tests']),
     description='A finite state machine library',
     long_description="""
